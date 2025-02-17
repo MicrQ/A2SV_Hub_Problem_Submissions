ee8ffe9cc6b3c0a4e651c62341e9fd38f725923f
Abenet Gebre <140071383+MicrQ@users.noreply.github.com>
2025-02-17 22:15:57 +0300

    Created 'Longest-consecutive-sequence/' by A2SV Hub

---
+++ 17-Feb-2025/Longest-consecutive-sequence/ 269698.py
@@ -0,0 +1,26 @@
+# Problem: Longest-consecutive-sequence/ - https://leetcode.com/problems/longest-consecutive-sequence/
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        
+        if not nums:
+            return 0
+
+        # sliding window :)
+        nums.sort()
+
+        max_consecutive = 1
+
+        current_consecutive = 1
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:
+                continue
+    
+            if nums[i] - nums[i - 1] == 1:
+                current_consecutive += 1
+                max_consecutive = max(current_consecutive, max_consecutive)
+
+            else:
+                current_consecutive = 1
+
+        return max_consecutive
