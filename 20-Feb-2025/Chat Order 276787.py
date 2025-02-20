5d15b301916678c87b6012314a1c5d435fe02dd0
Abenet Gebre <140071383+MicrQ@users.noreply.github.com>
2025-02-20 17:43:18 +0300

    Created 'Chat Order' by A2SV Hub

---
+++ 20-Feb-2025/Chat Order 276787.py
@@ -0,0 +1,30 @@
+# Problem: Chat Order - https://codeforces.com/problemset/problem/637/B
+
+# from datetime import datetime
+
+# n = int(input())
+
+# # marking each meessage with time stamp
+# chats = {input(): datetime.now() for i in range(n)}
+
+# # sorting the messages based on the time stamp
+# sorted_chats = sorted(chats.items(), key=lambda x: x[1], reverse=True)
+
+# # printing chats (recent to old)
+# for chat in sorted_chats:
+#     print(chat[0])
+
+from collections import defaultdict
+
+n = int(input())
+chats = [input() for i in range(n)]
+
+# used to memorize printed chats
+printed = defaultdict(bool)
+
+# looping through the chats from recent to old
+for chat in chats[::-1]:
+
+    if printed.get(chat) is None:
+        print(chat)
+        printed[chat] = True
