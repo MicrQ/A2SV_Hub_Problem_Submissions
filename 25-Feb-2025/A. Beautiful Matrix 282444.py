d29713b4a91e6b243504ee8337a6ced468a7dd2e
Abenet Gebre <140071383+MicrQ@users.noreply.github.com>
2025-02-25 16:01:59 +0300

    Created 'A. Beautiful Matrix' by A2SV Hub

---
+++ 25-Feb-2025/A. Beautiful Matrix 282444.py
@@ -0,0 +1,15 @@
+# Problem: A. Beautiful Matrix - https://codeforces.com/problemset/problem/263/A
+
+matrix = [[int(bit) for bit in input().split()]
+          for _ in range(5)]
+
+row = 0
+
+for i in range(5):
+    if 1 in matrix[i]:
+        row = i
+        break
+
+col = matrix[row].index(1)
+
+print(abs(2 - row) + abs(2 - col))
