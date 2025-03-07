162b898607004d1fb872342e3b7678d7053632ee
Abenet Gebre <140071383+MicrQ@users.noreply.github.com>
2025-03-07 09:59:44 +0300

    Created 'Less or Equal' by A2SV Hub

---
+++ 07-Mar-2025/Less or Equal 295590.py
@@ -0,0 +1,18 @@
+# Problem: Less or Equal - https://codeforces.com/problemset/problem/977/C
+
+n, k = [int(num) for num in input().split()]
+nums = [int(num) for num in input().split()]
+
+nums.sort()
+
+if k == 0:
+    print(1 if nums[0] > 1 else -1)
+
+elif k >= n:
+    print(nums[-1])
+    
+elif nums[k] == nums[k - 1]:
+    print(-1)
+    
+else:
+    print(nums[k - 1])
