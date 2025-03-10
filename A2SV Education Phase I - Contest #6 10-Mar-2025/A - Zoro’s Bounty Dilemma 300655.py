7755548fd518bb723c4c8967de9ed390f7faaf27
Abenet Gebre <140071383+MicrQ@users.noreply.github.com>
2025-03-10 21:11:22 +0300

    Created 'A - Zoro’s Bounty Dilemma' by A2SV Hub

---
+++ A2SV Education Phase I - Contest #6 10-Mar-2025/A - Zoro’s Bounty Dilemma 300655.py
@@ -0,0 +1,20 @@
+# Problem: A - Zoro’s Bounty Dilemma - https://codeforces.com/gym/594356/problem/A
+
+tests = int(input())
+
+for test in range(tests):
+
+    s = input()
+
+    res = ''
+
+    for char in s:
+        if not res and char != '=':
+            res = char
+
+        elif res and char != res and char != '=':
+            print('?')
+            break
+
+    else:
+        print(res if res else '=')
