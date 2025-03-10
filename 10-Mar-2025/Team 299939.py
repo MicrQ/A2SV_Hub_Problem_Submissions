37fda47345798ab1ac980feffdb64cd4f6be58bf
Abenet Gebre <140071383+MicrQ@users.noreply.github.com>
2025-03-10 12:36:59 +0300

    Created 'Team' by A2SV Hub

---
+++ 10-Mar-2025/Team 299939.py
@@ -0,0 +1,11 @@
+# Problem: Team - https://codeforces.com/contest/231/problem/A
+
+n = int(input())
+
+count = 0
+for _ in range(n):
+
+    if input().count('1') >= 2:
+        count += 1
+        
+print(count)
