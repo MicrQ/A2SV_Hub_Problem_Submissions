940a01f78f9197e8418b604c09898dc0d439e04d
Abenet Gebre <140071383+MicrQ@users.noreply.github.com>
2025-03-12 23:14:03 +0300

    Created 'B - Thousand Sunny's Network Setup' by A2SV Hub

---
+++ A2SV Education Phase I - Contest #6 12-Mar-2025/B - Thousand Sunny's Network Setup 304468.py
@@ -0,0 +1,7 @@
+# Problem: B - Thousand Sunny's Network Setup - https://codeforces.com/gym/594356/problem/B
+
+n, k = [int(num) for num in input().split()]
+speeds = [int(num) for num in input().split()]
+
+speeds.sort()
+print(speeds[n - k])
