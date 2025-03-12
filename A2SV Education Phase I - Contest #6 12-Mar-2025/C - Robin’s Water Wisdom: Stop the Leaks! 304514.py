76b2e561f7dac374b5b4361f7a25bdabca304352
Abenet Gebre <140071383+MicrQ@users.noreply.github.com>
2025-03-12 23:56:39 +0300

    Created 'C - Robin’s Water Wisdom: Stop the Leaks!' by A2SV Hub

---
+++ A2SV Education Phase I - Contest #6 12-Mar-2025/C - Robin’s Water Wisdom: Stop the Leaks! 304514.py
@@ -0,0 +1,20 @@
+# Problem: C - Robin’s Water Wisdom: Stop the Leaks! - https://codeforces.com/gym/594356/problem/C
+
+n, A, B = [int(num) for num in input().split()]
+nums = [int(num) for num in input().split()]
+
+target = nums[0]
+sum_ = sum(nums)
+nums = sorted(nums[1:])
+
+
+# 2 # 1,3,4,5
+blocked = 0
+for i in range(n - 2, -1, -1):
+    if target/sum_ * A >= B:
+        break
+
+    sum_ -= nums[i]
+    blocked += 1
+
+print(blocked)
