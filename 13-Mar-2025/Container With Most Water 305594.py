9b49a640665cadf844d1b12c2a85e1c51dc57c5f
Abenet Gebre <140071383+MicrQ@users.noreply.github.com>
2025-03-13 20:58:54 +0300

    Created 'Container With Most Water' by A2SV Hub

---
+++ 13-Mar-2025/Container With Most Water 305594.py
@@ -0,0 +1,23 @@
+# Problem: Container With Most Water - https://leetcode.com/problems/container-with-most-water/
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        
+        # using two pointers
+        left = 0
+        right = len(height) - 1
+
+        max_size = 0
+
+        while left < right:
+
+            # identify the min
+            min_ = min(height[left], height[right])
+            max_size = max(max_size, min_ * (right - left))
+
+            if height[left] == min_:
+                left += 1
+            else:
+                right -= 1
+
+        return max_size
