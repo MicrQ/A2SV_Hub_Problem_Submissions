42bee2d6c020fc9447b3c8982b1f036b30bb7997
Abenet Gebre <140071383+MicrQ@users.noreply.github.com>
2025-04-14 21:53:41 +0300

    Created 'C - z-sort' by A2SV Hub

---
+++ A2SV Remote Education Contest #9 14-Apr-2025/C - z-sort 333909.py
@@ -0,0 +1,23 @@
+# Problem: C - z-sort - https://codeforces.com/gym/603156/problem/C
+
+n = int(input())
+a = [int(num) for num in input().split()]
+
+a.sort()
+answer = []
+left, right = 0, n - 1
+lturn = True
+
+while left <= right:
+
+    if lturn:
+        answer.append(a[left])
+        left += 1
+
+    else:
+        answer.append(a[right])
+        right -= 1
+
+    lturn = not lturn
+
+print(*answer)
