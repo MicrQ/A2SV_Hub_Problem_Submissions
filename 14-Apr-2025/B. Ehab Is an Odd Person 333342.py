5d5accb88efcf4b204f1a0fd364b81720c7b5866
Abenet Gebre <140071383+MicrQ@users.noreply.github.com>
2025-04-14 17:15:19 +0300

    Created 'B. Ehab Is an Odd Person' by A2SV Hub

---
+++ 14-Apr-2025/B. Ehab Is an Odd Person 333342.py
@@ -0,0 +1,12 @@
+# Problem: B. Ehab Is an Odd Person - https://codeforces.com/problemset/problem/1174/B
+
+n = int(input())
+a = [int(num) for num in input().split()]
+
+evens = any(x % 2 == 0 for x in a)
+odds = any(x % 2 == 1 for x in a)
+
+if evens and odds:
+    a.sort()
+    
+print(*a)
