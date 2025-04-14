1e33846863489f584429b0530e49c3772e3efe19
Abenet Gebre <140071383+MicrQ@users.noreply.github.com>
2025-04-14 20:48:43 +0300

    Created 'A - The Rules of the Bus' by A2SV Hub

---
+++ A2SV Remote Education Contest #9 14-Apr-2025/A - The Rules of the Bus 333844.py
@@ -0,0 +1,23 @@
+# Problem: A - The Rules of the Bus - https://codeforces.com/gym/603156/problem/A
+
+tests = int(input())
+
+for _ in range(tests):
+
+    n = int(input())
+    seats = [int(num) for num in input().split()]
+
+    captured = set()
+    captured.add(seats[0])
+
+    for i in range(1, n):
+
+        seat = seats[i]
+        if seat - 1 not in captured and seat + 1 not in captured:
+            print('NO')
+            break
+
+        captured.add(seat)
+
+    else:
+        print('YES')
