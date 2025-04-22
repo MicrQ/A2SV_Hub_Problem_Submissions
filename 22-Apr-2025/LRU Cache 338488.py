5863d5cc332dab6c404fb8a9ee0ade00cf6f37f5
Abenet Gebre <140071383+MicrQ@users.noreply.github.com>
2025-04-22 13:37:33 +0300

    Created 'LRU Cache' by A2SV Hub

---
+++ 22-Apr-2025/LRU Cache 338488.py
@@ -0,0 +1,59 @@
+# Problem: LRU Cache - https://leetcode.com/problems/lru-cache/
+
+class Node():
+    def __init__(self, key: int, val: int):
+        self.key = key
+        self.val = val
+        self.next = None
+        self.prev = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+        self.left = Node(0, 0)
+        self.right = Node(0, 0)
+        self.left.next = self.right
+        self.right.prev = self.left
+
+    def get(self, key: int) -> int:
+
+        if not key in self.cache:
+            return -1
+
+        node = self.cache.get(key)
+        self.deleteNode(node)
+        self.insertNode(node)
+
+        return node.val
+
+    def put(self, key: int, value: int) -> None:
+
+        if key in self.cache:
+            self.deleteNode(self.cache[key])
+
+        node = Node(key, value)
+        self.cache[key] = node
+        self.insertNode(node)
+
+        if len(self.cache) > self.capacity:
+            del self.cache[self.left.next.key]
+            self.deleteNode(self.left.next)
+
+    def deleteNode(self, node: Node) -> None:
+
+        prev, next_ = node.prev, node.next
+        prev.next, next_.prev = next_, prev
+        node.next = node.prev = None
+
+    def insertNode(self, node: Node) -> None:
+
+        prev, next_ = self.right.prev, self.right
+        prev.next = next_.prev = node
+        node.next, node.prev = next_, prev
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
