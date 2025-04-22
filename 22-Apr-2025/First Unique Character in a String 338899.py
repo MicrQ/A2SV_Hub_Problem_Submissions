9d05695d84bf158aaf3380fcd1915f8e1df60acd
Abenet Gebre <140071383+MicrQ@users.noreply.github.com>
2025-04-22 21:55:30 +0300

    Created 'First Unique Character in a String' by A2SV Hub

---
+++ 22-Apr-2025/First Unique Character in a String 338899.py
@@ -0,0 +1,13 @@
+# Problem: First Unique Character in a String - https://leetcode.com/problems/first-unique-character-in-a-string/description/?envType=problem-list-v2&envId=queue
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        
+        s_frqs = Counter(s)
+
+        for i in range(len(s)):
+    
+            if s_frqs[s[i]] == 1:
+                return i
+
+        return -1
