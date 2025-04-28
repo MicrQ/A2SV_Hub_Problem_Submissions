c208a79368a1f6ad3418ad2beb5a8a2fef8bc0cd
Abenet Gebre <140071383+MicrQ@users.noreply.github.com>
2025-04-28 23:23:09 +0300

    Created 'E - NATOLI's String Quest' by A2SV Hub

---
+++ A2SV Contest #11 28-Apr-2025/E - NATOLI's String Quest 344376.py
@@ -0,0 +1,26 @@
+# Problem: E - NATOLI's String Quest - https://codeforces.com/gym/605795/problem/E
+
+s = input()
+t = []
+u = []
+n = len(s)
+
+mins = [''] * n
+mins[-1] = s[-1]
+
+for i in range(n - 2, -1, -1):
+    mins[i] = min(s[i], mins[i + 1])
+
+for i in range(n):
+    while t and t[-1] <= mins[i]:
+        u.append(t.pop())
+
+    if s[i] == mins[i]:
+        u.append(s[i])
+    else:
+        t.append(s[i])
+
+while t:
+    u.append(t.pop())
+
+print(''.join(u))
