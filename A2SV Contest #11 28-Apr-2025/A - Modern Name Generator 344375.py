396e86a10cc593086a483d84a875370da4bd7fe7
Abenet Gebre <140071383+MicrQ@users.noreply.github.com>
2025-04-28 23:22:53 +0300

    Created 'A - Modern Name Generator' by A2SV Hub

---
+++ A2SV Contest #11 28-Apr-2025/A - Modern Name Generator 344375.py
@@ -0,0 +1,9 @@
+# Problem: A - Modern Name Generator - https://codeforces.com/gym/605795/problem/A
+
+tests = int(input())
+
+for _ in range(tests):
+
+    name = ''.join([s[0] for s in input().split()])
+
+    print(name)
