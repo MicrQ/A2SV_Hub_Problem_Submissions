b8f66222c5e39407c2ae831b537cb1216e0d787c
Abenet Gebre <140071383+MicrQ@users.noreply.github.com>
2025-06-02 14:35:23 +0300

    Created 'A - Mirror Magic' by A2SV Hub

---
+++ A2SV Remote Education Contest #16 02-Jun-2025/A - Mirror Magic 362772.py
@@ -0,0 +1,12 @@
+# Problem: A - Mirror Magic - https://codeforces.com/gym/613405/problem/A
+
+tests = int(input())
+
+for _ in range(tests):
+
+    a = input()
+    maps = {'p': 'q', 'q': 'p', 'w': 'w'}
+
+    b = ''.join(maps[c] for c in reversed(a))
+
+    print(b)
