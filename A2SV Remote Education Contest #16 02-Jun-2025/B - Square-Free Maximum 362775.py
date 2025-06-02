53ecc8413c133093d1360557b7f0f6f6abcc0572
Abenet Gebre <140071383+MicrQ@users.noreply.github.com>
2025-06-02 15:00:02 +0300

    Created 'B - Square-Free Maximum' by A2SV Hub

---
+++ A2SV Remote Education Contest #16 02-Jun-2025/B - Square-Free Maximum 362775.py
@@ -0,0 +1,22 @@
+# Problem: B - Square-Free Maximum - https://codeforces.com/gym/613405/problem/B
+
+from math import isqrt
+
+
+n = int(input())
+a = [int(num) for num in input().split()]
+
+def isPerfect(x):
+    """ checks if a number is perfect """
+    if x < 0:
+        return False
+
+    root = isqrt(x)
+    return root * root == x
+
+largest = float('-inf')
+for num in a:
+    if not isPerfect(num):
+        largest = max(largest, num)
+
+print(largest)
