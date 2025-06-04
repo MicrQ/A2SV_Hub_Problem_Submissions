a98d379e7dc03f7aa6d9d409a1479bad75febc28
Abenet Gebre <140071383+MicrQ@users.noreply.github.com>
2025-06-04 22:54:00 +0300

    Created 'Combinations' by A2SV Hub

---
+++ 04-Jun-2025/Combinations 363768.py
@@ -0,0 +1,17 @@
+# Problem: Combinations - https://leetcode.com/problems/combinations/
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        ans = []
+        def backtrack(start, pair):
+            if len(pair) == k:
+                ans.append(pair[:])
+                return
+
+            for i in range(start, n + 1):
+                pair.append(i)
+                backtrack(i + 1, pair)
+                pair.pop()
+
+        backtrack(1, [])
+        return ans
