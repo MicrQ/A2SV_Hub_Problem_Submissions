fa19f4f54019856277a63d7aa572a772f60adc45
Abenet Gebre <140071383+MicrQ@users.noreply.github.com>
2025-06-05 22:56:51 +0300

    Created 'Permutations' by A2SV Hub

---
+++ 05-Jun-2025/Permutations 364132.py
@@ -0,0 +1,24 @@
+# Problem: Permutations - https://leetcode.com/problems/permutations/
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        n = len(nums)
+        frqs = Counter(nums)
+
+        def helper(perms, count):
+            if count == n:
+                res.append(perms[:])
+                return
+
+            for num in frqs:
+                if frqs[num] > 0:
+                    perms.append(num)
+                    frqs[num] -= 1
+
+                    helper(perms, count + 1)
+                    perms.pop()
+                    frqs[num] += 1
+
+        helper([], 0)
+        return res
