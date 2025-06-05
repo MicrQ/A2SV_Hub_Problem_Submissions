a1fa59934fb31cd7cbdbad7f0852387c86baba40
Abenet Gebre <140071383+MicrQ@users.noreply.github.com>
2025-06-05 21:21:03 +0300

    Created 'Combination Sum' by A2SV Hub

---
+++ 05-Jun-2025/Combination Sum 364018.py
@@ -0,0 +1,23 @@
+# Problem: Combination Sum - https://leetcode.com/problems/combination-sum/
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        n = len(candidates)
+
+        def helper(start, combs, total):
+            if total == target:
+                res.append(combs[:])
+                return
+
+            if start >= n or total > target:
+                return
+
+            combs.append(candidates[start])
+            helper(start, combs, total + candidates[start])
+            combs.pop()
+
+            helper(start + 1, combs, total)
+
+        helper(0, [], 0)
+        return res
