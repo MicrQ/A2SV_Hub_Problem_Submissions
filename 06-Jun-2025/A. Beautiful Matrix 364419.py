ca59ac39b315adfe73a0b80d911438b665653d4f
Abenet Gebre <140071383+MicrQ@users.noreply.github.com>
2025-06-07 00:42:25 +0300

    Created 'A. Beautiful Matrix' by A2SV Hub

---
+++ 06-Jun-2025/A. Beautiful Matrix 364419.py
@@ -0,0 +1,14 @@
+# Problem: A. Beautiful Matrix - https://codeforces.com/problemset/problem/263/A
+
+matrix = [
+    [int(num) for num in input().split()]
+    for _ in range(5)
+]
+
+for row in range(5):
+    for col in range(5):
+        if matrix[row][col] == 1:
+            one_row, one_col = row, col
+            break
+
+print(abs(one_row - 2) + abs(one_col - 2))
