ff2e31deb1255b75aec0c17a313c7411d049c1b1
Abenet Gebre <140071383+MicrQ@users.noreply.github.com>
2025-06-26 01:05:53 +0300

    Created 'Course Schedule' by A2SV Hub

---
+++ 25-Jun-2025/Course Schedule 370589.py
@@ -0,0 +1,33 @@
+# Problem: Course Schedule - https://leetcode.com/problems/course-schedule/
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = defaultdict(list)
+        visited = set()
+        visiting = set()
+
+        for course, prerequisite in prerequisites:
+            graph[course].append(prerequisite)
+
+        def dfs(course):
+            if course in visiting:
+                return False
+            if course in visited:
+                return True
+
+            visiting.add(course)
+            for pre in graph[course]:
+                if pre not in visited:
+                    if not dfs(pre):
+                        return False
+
+            visiting.remove(course)
+            visited.add(course)
+
+            return True
+
+        for course in range(numCourses):
+            if not dfs(course):
+                return False
+
+        return True
