dbf314595b5201904ae879466980f0d68fd34424
Abenet Gebre <140071383+MicrQ@users.noreply.github.com>
2025-07-28 02:39:48 +0300

    Created 'Find All Possible Recipes From Given Supplies' by A2SV Hub

---
+++ 27-Jul-2025/Find All Possible Recipes From Given Supplies 379930.py
@@ -0,0 +1,32 @@
+# Problem: Find All Possible Recipes From Given Supplies - https://leetcode.com/problems/find-all-possible-recipes-from-given-supplies/
+
+class Solution:
+    def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
+        n = len(recipes)
+        graph = defaultdict(list)
+        in_degree = defaultdict(int)
+        res = []
+
+        # building the dependency graph
+        for recipe, ings in zip(recipes, ingredients):
+            for ing in ings:
+                graph[ing].append(recipe)
+                in_degree[recipe] += 1
+
+        # kahn's algorithm :)
+        queue = deque(supplies)
+        recipe_set = set(recipes)
+
+        while queue:
+            item = queue.popleft()
+
+            if item in recipe_set:
+                res.append(item)
+
+            for nei in graph[item]:
+                in_degree[nei] -= 1
+
+                if in_degree[nei] == 0:
+                    queue.append(nei)
+
+        return res
